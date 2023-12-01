d3733da90e8ee387dffaadc18d113326bdd97555
Owner <Owner@MSI>
2023-12-01 11:41:55 +0900

    add fscanf

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<process.h>
+#include<assert.h>
 int main() {
+	const char* fileName = "data.txt";
+	FILE* fp = fopen(fileName, "r");
+	assert(fp);
 
-	return 0;
+	char number[] = "0";
+	while (fscanf(fp, "%s", number) != EOF) {
+		for (int i = 0; number[i] != '\0'; i++) {
+			if (number[i] == '1') {
+				printf("–");
+			}
+			else {
+				printf("@");
+			}
+		}
+		printf("\n");
+	}
+
+	fclose(fp);
+	system("pause>0");
 }
