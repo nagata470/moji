4097344517d61da8d90a1135d1e243f21786590b
Owner <Owner@MSI>
2023-12-02 11:33:13 +0900

    add c++ ver

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//exam
+#if 0
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<process.h>
@@ -7,10 +9,10 @@ int main() {
 	FILE* fp = fopen(fileName, "r");
 	assert(fp);
 
-	char number[] = "0";
-	while (fscanf(fp, "%s", number) != EOF) {
-		for (int i = 0; number[i] != '\0'; i++) {
-			if (number[i] == '1') {
+	char c[128];
+	while (fscanf(fp, "%s", c) != EOF) {
+		for (int i = 0; c[i] != '\0'; i++) {
+			if (c[i] == '1') {
 				printf("–");
 			}
 			else {
@@ -23,3 +25,40 @@ int main() {
 	fclose(fp);
 	system("pause>0");
 }
+#endif
+//C++ ver
+#if 1
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<fstream>
+#include<process.h>
+#include<assert.h>
+int main() {
+	std::ifstream fin("data.txt");
+
+	std::string c;
+	fin >> c;
+	while (!fin.eof()) {
+		for (int i = 0; c[i] != '\0'; i++) {
+			if (c[i] == '1') {
+				std::cout << "–";
+			}
+			else {
+				std::cout << "@";
+			}
+		}
+		std::cout << std::endl;
+		fin >> c;
+	}
+	for (int i = 0; c[i] != '\0'; i++) {
+		if (c[i] == '1') {
+			std::cout << "–";
+		}
+		else {
+			std::cout << "@";
+		}
+	}
+	std::cout << std::endl;
+	system("pause>0");
+}
+#endif
